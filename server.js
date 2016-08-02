0541da00ba3c84cc1280aa6e3f086609517cffed
DevDooly <sunhongyi@gmail.com>
2016-08-02 05:40:59 +0000

    chatting

---
+++ server.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get("/", function(req, res) {
+    res.sendfile("client.html");
+});
+
+var count = 1;
+var userList = [];
+
+io.on('connection', function(socket) {
+    console.log('user connected: ', socket.id);
+
+    var name = "user" + count++;
+
+    io.to(socket.id).emit('set name', name);
+
+    userList.push(name);
+    socket.broadcast.emit('user connect', {
+        name: name,
+        userList: userList
+    });
+
+    // io.to(socket.id).emit('change name', name);
+    socket.emit('welcome', {
+        name: name,
+        userList: userList
+    });
+
+    socket.on('disconnect', function() {
+        console.log('user disconnected: ', socket.id);
+
+        var i = userList.indexOf(name);
+        userList.splice(i, 1);
+
+        socket.broadcast.emit('left', {
+            name: name,
+            userList: userList
+        });
+    });
+
+    socket.on('send message', function(aftername, text) {
+        if (name != aftername) {
+            socket.broadcast.emit('change name', {
+              name : name,
+              aftername : aftername
+            });
+            name = aftername;
+        }
+
+        var msg = name + ' : ' + text;
+        console.log(msg);
+        io.emit('receive message', msg);
+    });
+});
+
+http.listen('50000', function() {
+    console.log("server on!");
+});
